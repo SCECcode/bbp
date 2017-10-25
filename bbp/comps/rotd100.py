e3f9b8f1343d0e3a69f56da7b3179ea66df747b7
Kevin Milner <kmilner@usc.edu>
2017-10-25 16:48:15 -0700

    dumb way of RD calc with duplication

--- bbp/comps/rotd100.py
+++ bbp/comps/rotd100.py
@@ -91,9 +91,9 @@ class RotD100(object):
         # Restore working directory
         os.chdir(old_cwd)
 
-    def __init__(self, i_r_srcfile, i_r_stations,
-                 i_a_obsdir, i_obs_format, i_obs_corr,
-                 i_mag, i_comparison_label, cutoff=None, sim_id=0):
+    def __init__(self, i_r_stations, i_r_srcfile=None,
+                 i_a_obsdir=None, i_obs_format=None, i_obs_corr=None,
+                 i_mag=None, i_comparison_label=None, cutoff=None, sim_id=0):
         """
         Initializes class variables
         """
@@ -109,7 +109,7 @@ class RotD100(object):
         self.src_keys = None
 
         # Make observed seismograms are in a format we can handle
-        if i_obs_format not in SUPPORTED_OBS_FORMATS:
+        if i_obs_format is not None and i_obs_format not in SUPPORTED_OBS_FORMATS:
             raise bband_utils.ParameterError("Format %s for " %
                                              (self.obs_format) +
                                              "observed seismograms "
@@ -550,6 +550,125 @@ class RotD100(object):
                      cutoff=self.max_cutoff, mode=plot_mode, colorset='single')
 
     def run(self):
+        print("RotD100".center(80, '-'))
+        #
+        # convert input bbp acc files to peer format acc files
+        #
+
+        install = install_cfg.InstallCfg.getInstance()
+        sim_id = self.sim_id
+        sta_base = os.path.basename(os.path.splitext(self.r_stations)[0])
+        self.log = os.path.join(install.A_OUT_LOG_DIR, str(sim_id),
+                                "%d.rotd100_%s.log" % (sim_id, sta_base))
+        a_statfile = os.path.join(install.A_IN_DATA_DIR,
+                                  str(sim_id),
+                                  self.r_stations)
+        a_tmpdir = os.path.join(install.A_TMP_DATA_DIR, str(sim_id))
+        a_outdir = os.path.join(install.A_OUT_DATA_DIR, str(sim_id))
+
+        #
+        # Make sure the tmp and out directories exist
+        #
+        bband_utils.mkdirs([a_tmpdir, a_outdir], print_cmd=False)
+
+        slo = StationList(a_statfile)
+        site_list = slo.getStationList()
+
+        for site in site_list:
+            stat = site.scode
+            print("==> Processing station: %s" % (stat))
+
+            # Since we have velocity files, we need to differentiate
+            # to get to acceleration
+
+            # Create path names and check if their sizes are within bounds
+            nsfile = os.path.join(a_tmpdir,
+                                  "%d.%s.000" % (sim_id, stat))
+            ewfile = os.path.join(a_tmpdir,
+                                  "%d.%s.090" % (sim_id, stat))
+            udfile = os.path.join(a_tmpdir,
+                                  "%d.%s.ver" % (sim_id, stat))
+            bbpfile = os.path.join(a_outdir,
+                                   "%d.%s.vel.bbp" % (sim_id, stat))
+
+            bband_utils.check_path_lengths([nsfile, ewfile, udfile],
+                                           bband_utils.GP_MAX_FILENAME)
+
+            cmd = ("%s/wcc2bbp " % (install.A_GP_BIN_DIR) +
+                   "wcc2bbp=0 nsfile=%s ewfile=%s udfile=%s < %s >> %s 2>&1" %
+                   (nsfile, ewfile, udfile, bbpfile, self.log))
+            bband_utils.runprog(cmd, abort_on_error=True, print_cmd=False)
+
+            for c in ["090", "000", "ver"]:
+                # Differentiate to get from velocity to accl needed by rotd100
+                # Create path names and check if their sizes are within bounds
+                filein = os.path.join(a_tmpdir,
+                                      "%d.%s.%s" %
+                                      (sim_id, stat, c))
+                fileout = os.path.join(a_tmpdir,
+                                       "%d.%s.acc.%s" %
+                                       (sim_id, stat, c))
+
+                bband_utils.check_path_lengths([filein, fileout],
+                                               bband_utils.GP_MAX_FILENAME)
+
+                cmd = ("%s/integ_diff diff=1 " % (install.A_GP_BIN_DIR) +
+                       "filein=%s fileout=%s >> %s 2>&1" %
+                       (filein, fileout, self.log))
+                bband_utils.runprog(cmd, abort_on_error=True, print_cmd=False)
+
+                # Check file length
+                bband_utils.check_path_lengths(["%s/%d.%s.acc.%s" %
+                                                (a_tmpdir, sim_id, stat, c)],
+                                               bband_utils.GP_MAX_FILENAME)
+
+            # Now we need to convert them back to bbp
+            # Create path names and check if their sizes are
+            # within bounds
+            nsfile = os.path.join(a_tmpdir,
+                                  "%d.%s.acc.000" % (sim_id, stat))
+            ewfile = os.path.join(a_tmpdir,
+                                  "%d.%s.acc.090" % (sim_id, stat))
+            udfile = os.path.join(a_tmpdir,
+                                  "%d.%s.acc.ver" % (sim_id, stat))
+            bbpfile = os.path.join(a_tmpdir,
+                                   "%d.%s.acc.bbp" % (sim_id, stat))
+
+            bband_utils.check_path_lengths([nsfile, ewfile, udfile],
+                                           bband_utils.GP_MAX_FILENAME)
+
+            cmd = ("%s/wcc2bbp " % (install.A_GP_BIN_DIR) +
+                   "nsfile=%s ewfile=%s udfile=%s " %
+                   (nsfile, ewfile, udfile) +
+                   "units=cm/s/s wcc2bbp=1 > %s 2>> %s" %
+                   (bbpfile, self.log))
+            bband_utils.runprog(cmd, abort_on_error=True, print_cmd=False)
+
+            # Now we need to convert to peer format
+            out_n_acc = os.path.join(a_tmpdir,
+                                     "%d.%s.peer_n.acc" % (sim_id, stat))
+            out_e_acc = os.path.join(a_tmpdir,
+                                     "%d.%s.peer_e.acc" % (sim_id, stat))
+            out_z_acc = os.path.join(a_tmpdir,
+                                     "%d.%s.peer_z.acc" % (sim_id, stat))
+            bbp_formatter.bbp2peer(bbpfile, out_n_acc, out_e_acc, out_z_acc)
+
+            # Let's have rotD50 create these output files
+            out_rotd100_base = "%d.%s.rd100" % (sim_id, stat)
+            tmp_rotd100 = os.path.join(a_tmpdir, out_rotd100_base)
+            out_rotd100 = os.path.join(a_outdir, out_rotd100_base)
+
+            # Run the rotD50 program
+            self.do_rotd100(a_tmpdir, out_e_acc, out_n_acc, out_z_acc,
+                           out_rotd100, self.log)
+
+            cmd = "cp %s %s" % (tmp_rotd100, out_rotd100)
+            bband_utils.runprog(cmd, abort_on_error=True, print_cmd=False)
+
+        # All done!
+        print("RotD100 Completed".center(80, '-'))
+
+    def runValidation(self):
         """
         Do all steps needed for creating the ratio of maximum to median
         response across orientations (RotD100/RotD50)
@@ -607,8 +726,14 @@ class RotD100(object):
 
 if __name__ == '__main__':
     print("Testing Module: %s" % (os.path.basename(sys.argv[0])))
-    ME = RotD100(sys.argv[1], sys.argv[2],
-                 sys.argv[3], sys.argv[4],
-                 sys.argv[5], sys.argv[6],
-                 sys.argv[7], sim_id=int(sys.argv[8]))
-    ME.run()
+    if len(sys.argv) == 3:
+        # simulation mode
+        ME = RotD100(sys.argv[1], sim_id=int(sys.argv[2]))
+        ME.run()
+    else:
+        # validation mode
+        ME = RotD100(sys.argv[2], sys.argv[1],
+                     sys.argv[3], sys.argv[4],
+                     sys.argv[5], sys.argv[6],
+                     sys.argv[7], sim_id=int(sys.argv[8]))
+        ME.run()
