6aaf796766dc8ed0a0570cf07fff402e5bc3e1ce
Fabio Silva <fsilva@usc.edu>
2019-04-30 10:09:17 -0700

    Updated version number, acceptance tests inputs, and added new events and methods to the report_bbp_data script.

--- bbp/utils/batch/create_supertable.sh
+++ bbp/utils/batch/create_supertable.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-for method in gp sdsu ucsb exsim song irikura_recipe_m1; do
+for method in gp sdsu ucsb exsim song irikura_recipe_m1 irikura_recipe_m2; do
     cmd="combine_bbp_data.py ${method}-results-${1}.txt"
-    for event in ch ar whittier nps tottori niigata nr lomap landers rdl mineral saguenay; do
+    for event in ch ar whittier nps tottori niigata nr lomap landers chuetsu parkfield sansimeon iwate; do
 	cmd="${cmd} ${2}/${method}-${event}-50r"
     done;
     $cmd
 done;
 
-for method in gp sdsu ucsb exsim song irikura_recipe_m1; do
+for method in gp sdsu ucsb exsim song irikura_recipe_m1 irikura_recipe_m2; do
     report_bbp_data.py ${method}-results-${1}.txt > ${method}-part-a-table-${1}.txt
 done;
