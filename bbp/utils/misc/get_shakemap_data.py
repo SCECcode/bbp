b64bd5a10138a3c71c33e68dc63bc1ac6bf96f9d
Fabio Silva <fsilva@usc.edu>
2020-12-08 10:41:25 -0800

    added options to print output in different units

--- bbp/utils/misc/get_shakemap_data.py
+++ bbp/utils/misc/get_shakemap_data.py
@@ -17,6 +17,7 @@ limitations under the License.
 Program to calculate the pga and pgv of seismograms from a station
 list
 """
+from __future__ import division, print_function
 
 # Import Python modules
 import os
@@ -53,11 +54,19 @@ def calculate_peak(input_file):
     return math.sqrt(max_ns * max_ew)
 
 def main():
-    if len(sys.argv) < 3:
-        print "Usage: %s station_list output_dir" % (sys.argv[0])
-        sys.exit(0)
+    if len(sys.argv) < 3 or len(sys.argv) > 4:
+        print("Usage: %s station_list output_dir [pga|pgv|pga-log|pga-g|pga-g-log|pgv-log]" %
+              (sys.argv[0]))
+        sys.exit(-1)
     station_list = sys.argv[1]
     outdir = sys.argv[2]
+    output_mode = "all"
+    if len(sys.argv) == 4:
+        output_mode = sys.argv[3].lower()
+    if output_mode not in ["all", "pga", "pgv", "pga-log",
+                           "pga-g", "pga-g-log", "pgv-log"]:
+        print("Requested output mode is invalid!")
+        sys.exit(-1)
 
     station_file = open(station_list)
     for line in station_file:
@@ -70,12 +79,34 @@ def main():
         lon = float(tokens[0])
         lat = float(tokens[1])
         station = tokens[2]
+
+        # Calculate peak values
         vel_file = glob.glob("%s/*.%s.vel.bbp" % (outdir, station))[0]
         acc_file = glob.glob("%s/*.%s.acc.bbp" % (outdir, station))[0]
         vel_peak = calculate_peak(vel_file)
         acc_peak = calculate_peak(acc_file)
-        print ("%2.3f   %2.3f   %s   %7.5e   %7.5e" %
-               (lon, lat, station, vel_peak, acc_peak))
+
+        # Convert units if needed
+        if output_mode == "pga-log":
+            acc_peak = math.log10(acc_peak)
+        elif output_mode == "pga-g":
+            acc_peak = acc_peak / 980.665
+        elif output_mode == "pga-g-log":
+            acc_peak = math.log10(acc_peak / 980.665)
+        elif output_mode == "pgv-log":
+            vel_peak = math.log10(vel_peak)
+
+        # Print output
+        if output_mode == "all":
+            print("%2.3f   %2.3f   %s   %7.5e   %7.5e" %
+                  (lon, lat, station, vel_peak, acc_peak))
+        elif output_mode in ["pga", "pga-log", "pga-g", "pga-g-log"]:
+            print("%2.3f   %2.3f   %7.5e" % (lon, lat, acc_peak))
+        elif output_mode in ["pgv", "pgv-log"]:
+            print("%2.3f   %2.3f   %7.5e" % (lon, lat, vel_peak))
+        else:
+            print("Invalid output mode!")
+            sys.exit(-1)
 
     station_file.close()
 
