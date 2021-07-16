9e976dad7d673bae9771ac9653b438589939f977
Fabio Silva <fsilva@usc.edu>
2021-07-16 00:18:49 -0700

    Added support for FAS comparison, improved command-line interface

--- bbp/utils/batch/compare_bbp_runs.py
+++ bbp/utils/batch/compare_bbp_runs.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
-Copyright 2010-2020 University Of Southern California
+Copyright 2010-2021 University Of Southern California
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -24,6 +24,7 @@ import os
 import sys
 import glob
 import operator
+import argparse
 
 def compute_avg_bias(bfn):
     """
@@ -54,14 +55,61 @@ def compute_avg_bias(bfn):
     # Return bias
     return bias
 
-def compare_runs(top_dir, output_file=None):
+def print_results_psa(results_psa):
+    """
+    Print PSA results
+    """
+    # Print the results
+    print("Rank    Simulation    Average Bias(PSA)")
+    rank = 0
+    for key, val in sorted(results_psa.items(),
+                           key=operator.itemgetter(1)):
+        rank = rank + 1
+        print("%4d    %10s        %8f" % (rank, key, val))
+
+def print_results_fas(results_fas):
+    """
+    Print PSA and FAS results
+    """
+    # Print the results
+    print("Rank    Simulation    Average Bias(FAS)")
+    rank = 0
+    for key, val in sorted(results_fas.items(),
+                           key=operator.itemgetter(1)):
+        rank = rank + 1
+        print("%4d    %10s        %8f" % (rank, key, val))
+
+def print_results_combined(results_psa, results_fas):
+    """
+    Create comparison including PSA and FAS results
+    """
+    sorted_realizations = sorted(results_fas)
+    sorted_psa = sorted(results_psa.items(),
+                        key=operator.itemgetter(1))
+    sorted_fas = sorted(results_fas.items(),
+                        key=operator.itemgetter(1))
+    rank_psa = list(dict(sorted_psa).keys())
+    rank_fas = list(dict(sorted_fas).keys())
+
+    print("Simulation  Rank(PSA)  Rank(FAS)   Average Bias(PSA)   Average Bias(FAS)")
+    for realization in sorted_realizations:
+        print("%10s   %6d    %6d     %8f       %8f" % (realization,
+                                                       rank_psa.index(realization) + 1,
+                                                       rank_fas.index(realization) + 1,
+                                                       results_psa[realization],
+                                                       results_fas[realization]))
+        
+def compare_runs(top_dir, args, output_file=None):
     """
     For all sub-directories in top_dir, look for the output bias
     results, calculate stats, and rank it. The result goes to
     output_file, or to stdout if output_file is not provided.
     """
-    # Start with an empty list
-    results = {}
+    # Start with empty lists
+    results_psa = {}
+    results_fas = {}
+    fas = args.fas
+    psa_fas_comp = args.comp
 
     for item in os.listdir(top_dir):
         sim_dir = os.path.join(top_dir, item)
@@ -70,27 +118,67 @@ def compare_runs(top_dir, output_file=None):
         if not os.path.isdir(sim_dir):
             continue
 
-        files = glob.glob(os.path.join(sim_dir, "*rotd50.bias"))
-        if len(files) != 1:
-            # bias file not found
+        files_psa = glob.glob(os.path.join(sim_dir, "*rotd50.bias"))
+        files_fas = glob.glob(os.path.join(sim_dir, "*seas.bias"))
+        if len(files_psa) != 1:
+            # PSA bias file not found
+            continue
+        if fas and len(files_fas) != 1:
+            # FAS bias file not found
             continue
 
         # Calculate stats for this simulation and add to results
-        results[item] = compute_avg_bias(files[0])
+        results_psa[item] = compute_avg_bias(files_psa[0])
+        if fas:
+            results_fas[item] = compute_avg_bias(files_fas[0])
 
-    # All done, print the results
-    print("Simulation    Average Bias")
-    for key, val in sorted(results.items(),
-                           key=operator.itemgetter(1)):
-        print("%10s    %8f" % (key, val))
+    if not fas:
+        print_results_psa(results_psa)
+    else:
+        if psa_fas_comp:
+            # Combine FAS and PSA results
+            print_results_combined(results_psa, results_fas)
+        else:
+            print_results_fas(results_fas)
 
+def parse_arguments():
+    """
+    This function takes care of parsing the command-line arguments and
+    asking the user for any missing parameters that we need
+    """
+    parser = argparse.ArgumentParser(description="Compare BBP "
+                                     " realizations by reading "
+                                     " PSA and FAS bias files.")
+    parser.add_argument("--dir", "-d", dest="input_dir",
+                        required=True, help="input directory")
+    parser.add_argument("-o", "--output-dir",
+                        dest="output_dir", required=True,
+                        help="output directory")
+    parser.add_argument("--fas", action='store_true',
+                        help="Add FAS comparison")
+    parser.add_argument("--comp", action="store_true",
+                        help="Create PSA/FAS comparison")
+    parser.add_argument("-c", "--codebase", required=False,
+                        dest="codebase", default=None,
+                        help="method used for the simulation")
+    args = parser.parse_args()
+
+    return args
 
 #----------------------------------------------------------------------------
 # Main
 #----------------------------------------------------------------------------
 
-if len(sys.argv) != 2:
-    print("Usage: %s top_simulation_dir" % (os.path.basename(sys.argv[0])))
-    sys.exit(1)
+ARGS = parse_arguments()
 
-compare_runs(os.path.join(sys.argv[1], "Sims", "outdata"))
+TOP_INPUT_DIR = ARGS.input_dir
+if not os.path.isdir(TOP_INPUT_DIR):
+    print("Invalid input directory!")
+    sys.exit(-1)
+if not "Sims" in os.listdir(TOP_INPUT_DIR):
+    print("Please provide the top-level simulation directory!\n"
+          "This is the directory given to the cluster script")
+    sys.exit(-1)
+INPUT_OUTDATA = os.path.join(TOP_INPUT_DIR, "Sims", "outdata")
+
+compare_runs(INPUT_OUTDATA, ARGS)
