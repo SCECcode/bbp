e98d10bc9fca17c89a138d843572a6cbb2ba89cd
Fabio Silva <fsilva@usc.edu>
2022-01-30 01:17:32 -0800

    Updated FAS module to support scenario simulations.

--- bbp/comps/fas.py
+++ bbp/comps/fas.py
@@ -1,22 +1,36 @@
 #!/usr/bin/env python
 """
-Copyright 2010-2021 University Of Southern California
+BSD 3-Clause License
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+Copyright (c) 2021, University of Southern California
+All rights reserved.
 
- http://www.apache.org/licenses/LICENSE-2.0
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
 
-This module takes care of building a workflow using either user
-choices interactively, or an option file containing all needed
-parameters.
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+   contributors may be used to endorse or promote products derived from
+   this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+This module calculates FAS for both simulated and observed timeseries.
 """
 from __future__ import division, print_function
 
@@ -656,7 +670,7 @@ class FAS(object):
     """
     Implement FAS analisys for the Broadband Platform
     """
-    def __init__(self, i_r_stations, i_comparison_label, sim_id=0):
+    def __init__(self, i_r_stations, i_comparison_label=None, sim_id=0):
         """
         Initializes class variables
         """
@@ -795,6 +809,149 @@ class FAS(object):
         """
         print("FAS Calculation".center(80, '-'))
 
+        if self.comp_label is None:
+            self.run_scenario()
+        else:
+            self.run_validation()
+
+        print("FAS Calculation Completed".center(80, '-'))
+
+    def run_scenario(self):
+        """
+        Run FAS analysis codes for scenario simulations
+        """
+        install = install_cfg.InstallCfg.getInstance()
+        sim_id = self.sim_id
+        sta_base = os.path.basename(os.path.splitext(self.r_stations)[0])
+        self.log = os.path.join(install.A_OUT_LOG_DIR, str(sim_id),
+                                "%d.fas_%s.log" % (sim_id, sta_base))
+        a_statfile = os.path.join(install.A_IN_DATA_DIR,
+                                  str(sim_id),
+                                  self.r_stations)
+        a_tmpdir = os.path.join(install.A_TMP_DATA_DIR, str(sim_id))
+        a_tmpdir_seis = os.path.join(a_tmpdir, "obs_seis_%s" % (sta_base))
+        a_outdir = os.path.join(install.A_OUT_DATA_DIR, str(sim_id))
+        a_outdir_fas = os.path.join(a_outdir, "FAS")
+
+        #
+        # Make sure the tmp and out directories exist
+        #
+        bband_utils.mkdirs([a_tmpdir, a_outdir, a_outdir_fas], print_cmd=False)
+
+        slo = StationList(a_statfile)
+        site_list = slo.getStationList()
+
+        # Save current directory
+        old_cwd = os.getcwd()
+        os.chdir(a_tmpdir)
+
+        # Initial values
+        new_dt = 10000000.0
+        new_samples = 0
+
+        # Pre-processing, find lowest dt among all input timeseries
+        for site in site_list:
+            acc_file = "%d.%s.acc.bbp" % (sim_id, site.scode)
+
+            # Read DT from simulated file
+            input_syn_acc_file = os.path.join(a_outdir, acc_file)
+            syn_dt = read_bbp_dt(input_syn_acc_file)
+            new_dt = min(new_dt, syn_dt)
+
+        print("==> Target DT set to %.4f" % (new_dt))
+
+        syn_rs_array = []
+
+        print("==> Pre-processing step 1: matching dt...")
+        for site in site_list:
+
+            acc_file = "%d.%s.acc.bbp" % (sim_id, site.scode)
+
+            shutil.copy2(os.path.join(a_outdir, acc_file),
+                         os.path.join(a_tmpdir, acc_file))
+
+            # Read DT from input file
+            input_syn_acc_file = os.path.join(a_tmpdir, acc_file)
+            syn_dt = read_bbp_dt(input_syn_acc_file)
+
+            if syn_dt == new_dt:
+                input_rs_syn_acc_file = input_syn_acc_file
+            else:
+                input_rs_syn_acc_file = os.path.join(a_tmpdir,
+                                                     "rs-%s" %
+                                                     (acc_file))
+                self.resample_bbp_file(input_syn_acc_file,
+                                       input_rs_syn_acc_file,
+                                       site.scode,
+                                       syn_dt, new_dt, a_tmpdir)
+
+            # Keep list of files to be used later
+            syn_rs_array.append(input_rs_syn_acc_file)
+
+        print("==> Pre-processing step 2: matching samples...")
+        for (site,
+             input_rs_syn_acc_file) in zip(site_list,
+                                           syn_rs_array):
+            # Read samples
+            syn_samples = read_bbp_samples(input_rs_syn_acc_file)
+            new_samples = max(new_samples, syn_samples)
+
+        print("==> Target samples set to %d" % (new_samples))
+
+        for (site,
+             input_rs_syn_acc_file) in zip(site_list,
+                                           syn_rs_array):
+
+            acc_file = "%d.%s.acc.bbp" % (sim_id, site.scode)
+
+            print("==> Processing station: %s" % (site.scode))
+
+            # Make sure file has new_samples data points
+            syn_samples = read_bbp_samples(input_rs_syn_acc_file)
+
+            if syn_samples == new_samples:
+                input_final_syn_acc_file = input_rs_syn_acc_file
+            else:
+                input_final_syn_acc_file = os.path.join(a_tmpdir,
+                                                        "pad-%s" %
+                                                        (acc_file))
+                extra_points = new_samples - syn_samples
+                print("=====> Adding %d extra points to %s" %
+                      (extra_points, os.path.basename(input_rs_syn_acc_file)))
+                add_extra_points(input_rs_syn_acc_file,
+                                 input_final_syn_acc_file,
+                                 extra_points)
+
+            # Copy calculated acc file to tmpdata
+            print("=====> computing FAS for calculated seismogram... ", end="", flush=True)
+            t1 = time.time()
+            self.compute_fas(a_tmpdir, a_outdir_fas,
+                             os.path.basename(input_final_syn_acc_file),
+                             site.scode, output_prefix="%d." % (self.sim_id))
+            t2 = time.time()
+            print("%10.2f s" % (t2 - t1))
+
+            ## Create comparison plot
+            #max_syn_freq = 1.0 / (syn_dt * 2)
+            #hfreq = min(max_syn_freq, site.high_freq_corner)
+            #sim_file = os.path.join(a_outdir_fas,
+            #                        "%d.%s.smc8.smooth.fs.col" %
+            #                        (self.sim_id, site.scode))
+            #outfile = os.path.join(a_outdir_fas,
+            #                       "%d.%s.fas.plot.png" %
+            #                       (self.sim_id, site.scode))
+            #plot_fas_comparison(site.scode, sim_file,
+            #                    self.sim_id, outfile,
+            #                    lfreq=site.low_freq_corner,
+            #                    hfreq=hfreq)
+
+        # All done, restore working directory
+        os.chdir(old_cwd)
+
+    def run_validation(self):
+        """
+        Run FAS analysis codes for validation simulations
+        """
         install = install_cfg.InstallCfg.getInstance()
         sim_id = self.sim_id
         sta_base = os.path.basename(os.path.splitext(self.r_stations)[0])
@@ -974,8 +1131,6 @@ class FAS(object):
 
         # All done, restore working directory
         os.chdir(old_cwd)
-
-        print("FAS Calculation Completed".center(80, '-'))
 
 if __name__ == '__main__':
     if len(sys.argv) < 4:
