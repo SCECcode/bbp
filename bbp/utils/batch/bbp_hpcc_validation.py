ba47e15e48da18bc842f707504e9a18a14556df2
Fabio Silva <fsilva@usc.edu>
2022-02-06 14:00:18 -0800

    Copied setup_bbp_env.sh into setup_bbp_env.hpc

--- bbp/utils/batch/bbp_hpcc_validation.py
+++ bbp/utils/batch/bbp_hpcc_validation.py
@@ -1,18 +1,34 @@
 #!/usr/bin/env python
 """
-Copyright 2010-2020 University Of Southern California
+BSD 3-Clause License
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+Copyright (c) 2021, University of Southern California
+All rights reserved.
 
- http://www.apache.org/licenses/LICENSE-2.0
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+   contributors may be used to endorse or promote products derived from
+   this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 Program to set up a full validation run on USC's HPC
 """
@@ -430,6 +446,10 @@ def main():
                       help="Prefix of SRF files to use, "
                       "only for GP, SDSU and UCSB methods. "
                       "Simulations begin after the rupture generator.")
+    parser.add_option("--src", "--source", type="string", action="store",
+                      dest="source",
+                      help="source description that replaces "
+                      "one in validation")
     parser.add_option("--hypo-rand", action="store_true", dest="hyporand",
                       help="Enables hypocenter randomization")
     parser.add_option("--no-hypo-rand", action="store_false", dest="hyporand",
@@ -589,12 +609,15 @@ def main():
     else:
         hypo_rand = False
 
-    try:
-        source_file = val_obj.get_input(codebase, "source")
-    except KeyError:
-        print("Unable to get source file for event %s, codebase %s!" %
-              (event, codebase))
-        sys.exit(1)
+    if options.source is not None:
+        source_file = options.source
+    else:
+        try:
+            source_file = val_obj.get_input(codebase, "source")
+        except KeyError:
+            print("Unable to get source file for event %s, codebase %s!" %
+                  (event, codebase))
+            sys.exit(1)
     if not source_file:
         print("Source file for event %s, codebase %s not specified!" %
               (event, codebase))
