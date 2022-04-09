253e5378c1329883c165766f1d8fc034d998e5bb
Fabio Silva <fsilva@usc.edu>
2022-04-09 00:20:22 -0700

    check ci package versions

--- .github/scripts/bbp-build-ci.sh
+++ .github/scripts/bbp-build-ci.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
+echo $BBP_DIR
+
+python --version
+
+python3 --version
+
+python3 -c "import numpy; print(numpy.__version__)"
+
+python3 -c "import scipy; print(scipy.__version__)"
+
+python3 -c "import matplotlib; print(matplotlib.__version__)"
+
+python3 -c "import numba; print(numba.__version__)"
+
+python3 -c "import pyproj; print(pyproj.__version__)"
+
 ls
 
