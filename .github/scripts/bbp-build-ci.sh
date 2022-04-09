e4d3d12754a7b26aa23cd7c0afd4dc9f15928671
Fabio Silva <fsilva@usc.edu>
2022-04-09 01:03:25 -0700

    Compile source distribution

--- .github/scripts/bbp-build-ci.sh
+++ .github/scripts/bbp-build-ci.sh
@@ -20,7 +20,38 @@ python3 -c "import matplotlib; print('Matplotlib: ', matplotlib.__version__)"
 python3 -c "import numba; print('Numba: ', numba.__version__)"
 python3 -c "import pyproj; print('PyProj: ', pyproj.__version__)"
 
-echo $RUNNER_WORKSPACE
+# Set basic parameters
+VERSION=22.4.0
+BASEDIR="${RUNNER_WORKSPACE"
+BBPDIR="${BASEDIR}/bbp"
+SRCDIR="${BBPDIR}/src"
 
-ls $RUNNER_WORKSPACE
+# Create installation directories
+echo "=> Creating directory tree..."
+mkdir -p ${BASEDIR}/bbp_val
+mkdir -p ${BASEDIR}/bbp_gf
+mkdir -p ${BASEDIR}/bbp_data
+echo
 
+# Compile source distribution
+echo "=> Main Broadband Platform Source Distribution"
+echo "==> Compiling... (it may take a while)"
+OLD_DIR=`pwd`
+cd ${SRCDIR}
+make
+cd ${OLD_DIR}
+# Done with main source distribution
+echo "==> Installed!"
+
+# Install LABasin500 (CI) region and NR validation packages
+echo "==> LA Basin"
+cd ${BASEDIR}/bbp_gf
+
+# download_untar http://hypocenter.usc.edu/research/bbp/versions/${VERSION}/labasin500-velocity-model-${VERSION}.tar.gz ${MD5FILE}
+
+echo "==> NR"
+cd ${BASEDIR}/bbp_val
+
+# download_untar http://hypocenter.usc.edu/research/bbp/versions/${VERSION}/nr-validation-${VERSION}.tar.gz ${MD5FILE}
+
+echo "==> Completed!"
