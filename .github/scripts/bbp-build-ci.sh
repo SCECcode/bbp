54e539d2eb0cc7a9b801553db1865559defd525f
Fabio Silva <fsilva@usc.edu>
2022-04-09 00:53:35 -0700

    print Python package versions

--- .github/scripts/bbp-build-ci.sh
+++ .github/scripts/bbp-build-ci.sh
@@ -4,26 +4,23 @@ echo "==> Installing Python packages needed by BBP..."
 
 sudo apt install python3-numpy python3-scipy python3-matplotlib python3-numba python3-pyproj
 
-echo $BBP_DIR
+echo $BBP_BASE_DIR
 
-
+echo "======================GCC===================="
 gcc --version
 
+echo "===================GFORTRAN=================="
 gfortran --version
 
-python --version
-
+echo "===================Python 3=================="
 python3 --version
+python3 -c "import numpy; print('Numpy: ', numpy.__version__)"
+python3 -c "import scipy; print('SciPy: ', scipy.__version__)"
+python3 -c "import matplotlib; print('Matplotlib: ', matplotlib.__version__)"
+python3 -c "import numba; print('Numba: ', numba.__version__)"
+python3 -c "import pyproj; print('PyProj: ', pyproj.__version__)"
 
-python3 -c "import numpy; print(numpy.__version__)"
+echo $RUNNER_WORKSPACE
 
-python3 -c "import scipy; print(scipy.__version__)"
-
-python3 -c "import matplotlib; print(matplotlib.__version__)"
-
-python3 -c "import numba; print(numba.__version__)"
-
-python3 -c "import pyproj; print(pyproj.__version__)"
-
-ls
+ls $RUNNER_WORKSPACE
 
