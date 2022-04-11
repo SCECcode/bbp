ad76d69cd28792f65fff54ebc79fc2143d0e83b5
Fabio Silva <fsilva@usc.edu>
2022-04-11 01:48:42 -0700

    Added unit tests to CI

--- .github/scripts/bbp-test-ci.sh
+++ .github/scripts/bbp-test-ci.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
-echo `pwd`
+# Set basic parameters
+VERSION=22.4.0
+BASEDIR="${RUNNER_WORKSPACE}"
+BBPDIR="${BASEDIR}/bbp/bbp"
+SRCDIR="${BBPDIR}/src"
 
-ls
+BBP_DIR=${BBPDIR}
+BBP_GF_DIR=${BASEDIR}/bbp_gf
+BBP_VAL_DIR=${BASEDIR}/bbp_val
+PYTHONPATH=${BBPDIR}/comps:${PYTHONPATH}
+BBP_DATA_DIR=${BASEDIR}/bbp_data
+PATH=${BBPDIR}/comps:${BBPDIR}/utils/batch:$PATH
+
+echo
+echo "===> Running Unit Tests..."
+
+cd $BBP_DIR/tests
+./UnitTestsCI.py
+
 
