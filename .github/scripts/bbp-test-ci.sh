9dce4d11cebff3d216fc32c91af429d3cdf4e4c3
Fabio Silva <fsilva@usc.edu>
2022-04-12 02:07:44 -0700

    Added PySeismoSoil to PYTHONPATH

--- .github/scripts/bbp-test-ci.sh
+++ .github/scripts/bbp-test-ci.sh
@@ -9,7 +9,7 @@ SRCDIR="${BBPDIR}/src"
 export BBP_DIR=${BBPDIR}
 export BBP_GF_DIR=${BASEDIR}/bbp_gf
 export BBP_VAL_DIR=${BASEDIR}/bbp_val
-export PYTHONPATH=${BBPDIR}/comps:${PYTHONPATH}
+export PYTHONPATH=${BBPDIR}/comps:${BBPDIR}/comps/PySeismoSoil:${PYTHONPATH}
 export BBP_DATA_DIR=${BASEDIR}/bbp_data
 export PATH=${BBPDIR}/comps:${BBPDIR}/utils/batch:$PATH
 ulimit -s unlimited
